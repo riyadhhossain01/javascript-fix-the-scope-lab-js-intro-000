ef69727d772d6125df46f8998e62aa4dc1c995d1
riyadh hossain <riyadh_h@ymail.com>
2018-07-12 00:40:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ function add2(n) {
 
 }
 
-var theFunk =  function funkyFunction(){
+function funkyFunction() {
+  return function() {
     return "FUNKY!"
-  
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
